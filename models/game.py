6f3a650f944be3126bb4245e6be14f75cebc3d95
CarlyCoding <carly_off@outlook.com>
2022-07-09 15:52:32 +0100

    adding controller doc

--- models/game.py
+++ models/game.py
@@ -1,3 +1,4 @@
+from models.list_of_player_choices import Player2
 from models.player import Player
 
 class Game():
@@ -6,6 +7,11 @@ class Game():
             self.rock = _rock
             self.paper = _paper
             self.scissors = _scissors
+
+# This function will take in 2 players and compare their results, returning a string of the winning player
+        def _who_wins
+
+
 
 
 
