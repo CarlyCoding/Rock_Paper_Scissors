7ac3733ff6e3d3207485c2b01a9cd374bdecfb8b
CarlyCoding <carly_off@outlook.com>
2022-07-10 19:14:44 +0100

    All changes made, css and html completed

--- models/game.py
+++ models/game.py
@@ -23,5 +23,13 @@ class Game():
             # outside the if. 
             return (f'{self.player2.name} wins!')
 
+# Notes to improve in future after testing:
+
+# -If player two wins it doesn't return a formatted string as I have not yet coded this- it defaults to the P2 win code.  
+# -Flask has issues with the 'none' aka draw result. This requires reworking. 
+# -An alternative design choice would be a welcome page with a button to take to the game & result with PC opponent using a loop/ random func.
+# I would have coded in lower case for my css and html.
+# The URL is case sensitive for RPS choice- would add if == lower case option too. 
+
 
 
